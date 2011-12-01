46000e206cdf0a1a0fba92cb5c6ea6e05a934959
Mary Bucknell <mbucknell@usgs.gov>
2011-12-01 21:25:52 +0000

    Added staticfiles_urlpatterns to urlpatterns. This should allow django to serve the static files rather than apache for the development server.

--- nemi/nemi/urls.py
+++ nemi/nemi/urls.py
@@ -2,9 +2,12 @@
 
 
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import search.urls
 
 urlpatterns = patterns('',
     url(r'^search/', include(search.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
