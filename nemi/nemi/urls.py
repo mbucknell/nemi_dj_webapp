dd99c8e78053d333123af3d366f116cc70a4a4d7
Mary Bucknell <mbucknell@usgs.gov>
2011-12-02 14:32:42 +0000

    Moved Icon-Extlink.png to the style directory where it is used.  The apache server was not serving the file correctly otherwise. Some other minor changes where also made.

--- nemi/nemi/urls.py
+++ nemi/nemi/urls.py
@@ -1,13 +1,9 @@
 ''' Module includes all urls confs for the nemi project '''
 
-
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import search.urls
 
 urlpatterns = patterns('',
     url(r'^search/', include(search.urls)),
 )
-
-urlpatterns += staticfiles_urlpatterns()
