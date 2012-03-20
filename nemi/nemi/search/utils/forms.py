f8bd9d5c60dcd5ff222e1ca7dfaffe81c7791e6d
Mary Bucknell <mbucknell@usgs.gov>
2012-03-20 15:15:38 +0000

    Created a utils package in the nemi.search package which currently contains helper functions for forms and for views (view_utils). This package will contain utility functions that are used in the search application. There are two reasons for doing this. One is that it is easier to write tests for these helper functions. Second is that potentially some of these are general enough to go into a general purpose app. By segregating them now and writing unittests, it should be simple to migrate them to a different app or into their own app.
    There is more refactoring that could be done in the search app but this is a start.

---
+++ nemi/nemi/search/utils/forms.py
@@ -0,0 +1,53 @@
+'''
+Created on Mar 15, 2012
+
+@author: mbucknel
+'''
+
+# standard python packages
+import types
+
+def _get_choice_select(field):
+    '''Returns the visible choice from the ChoiceField field parameter. The field must in a form
+    be a bound field. The function assumes choice values are integer or string and retrieves
+    the value kind from the second element in the choices list. This allows for an all or None
+    selection as the first.
+    '''
+    if type(field.field.choices[1][0]) is types.IntType:
+        return dict(field.field.choices).get(int(field.data))
+    return dict(field.field.choices).get(field.data)
+
+def get_criteria(field):
+    '''Returns a tuple if the field value is not 'all', where the first element is the label
+    of field and the second element is the visible choice for field.
+    Assumes that the field is a ChoiceField and must be field in a form.
+    '''
+    if field.data == 'all':
+        return None
+    else:
+        return (field.label, _get_choice_select(field))
+    
+def get_criteria_from_field_data(form, field_name):
+    '''Returns a tuple representing field_name's label and data from form.
+    If the field's data value is none, the function returns None.
+    '''
+    if form.cleaned_data[field_name] == None:
+        return None
+    
+    else:
+        return (form[field_name].label, form.cleaned_data[field_name])
+    
+def get_multi_choice(form, name):
+    ''' Returns the list of selected choices in a MultipleChoiceField. The form must be validated and the
+    field is identified by name. If all are selected an empty list is returned.
+    This function works for choice field values which are integer or string.
+    '''
+    choice_dict = dict(form[name].field.choices)
+    if len(form.cleaned_data[name]) == len(choice_dict):
+        return []
+    else:
+        if type(choice_dict.keys()[0]) is types.IntType:
+            return [choice_dict.get(int(k)) for k in form.cleaned_data[name]]
+        else:
+            return [choice_dict.get(k) for k in form.cleaned_data[name]]
+    
---
+++ nemi/nemi/search/utils/view_utils.py
@@ -0,0 +1,56 @@
+'''
+Created on Mar 20, 2012
+
+@author: mbucknel
+'''
+# Provides conversion to Excel format
+from xlwt import Workbook
+
+from django.http import HttpResponse
+
+def dictfetchall(cursor):
+    '''Returns all rows from the cursor query as a dictionary with the key value equal to column name in uppercase'''
+    desc = cursor.description
+    return [
+            dict(zip([col[0] for col in desc], row))
+            for row in cursor.fetchall()
+            ]
+def tsv_response(headings, vl_qs, filename):
+    ''' Returns an http response which contains a tab-separate-values file 
+    representing the values list query set, vl_qs, and using headings as the 
+    column headers. filename will be the name of the file created with the suffix *.tsv
+    '''
+    response = HttpResponse(mimetype='text/tab-separated-values')
+    response['Content-Disposition'] = ('attachment; filename=%s.tsv' % filename)
+
+    response.write('\t'.join(headings))
+    response.write('\n')
+
+    for row in vl_qs:
+        for col in row:
+            response.write('%s\t' % str(col))
+        response.write('\n')
+
+    return response
+
+def xls_response(headings, vl_qs, filename):
+    '''Returns an http response which contains an Excel file
+    representing the values list query set, vl_qs, and using headings
+    as the column headers. filename will be the name of the file created with the suffix *.xls
+    '''
+    response = HttpResponse(mimetype='application/vnd.ms-excel')
+    response['Content-Disposition'] = ('attachment; filename=%s.xls' % filename)
+    
+    wb = Workbook()
+    ws = wb.add_sheet('sheet 1')
+    
+    for col_i in range(len(headings)):
+        ws.write(0, col_i, headings[col_i])
+    
+    for row_i in range(len(vl_qs)):
+        for col_i in range(len(vl_qs[row_i])):
+            ws.write(row_i + 1, col_i, vl_qs[row_i][col_i])
+    
+    wb.save(response)
+    
+    return response
