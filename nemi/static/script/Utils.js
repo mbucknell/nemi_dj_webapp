5389d15c3bc79aff0e6a917daa9bfe7a4cc2ba84
Mary Bucknell <mbucknell@usgs.gov>
2013-03-18 15:12:08 +0000

    Added code to make allow only one of the analyte name or code fields to be enabled so that it is clear which one the search is using.
    Also changed the color of the Limit by section's bar so it's not as distinct as the Filtered Search and General Search sections.

--- nemi/static/script/Utils.js
+++ nemi/static/script/Utils.js
@@ -1,19 +1,18 @@
 Utils = {
 	setEnabled: function(els /* jquery elements */, isEnabled /*Boolean */){
 		/* If isEnabled is true, remove the disabled attribute and class from els.
-		 * If false, add the attribute anc class to els. For any of the els which are inputs
-		 * find their associated labels and remove/add the disabled class as appropriate.
+		 * If false, add the attribute and class to els. For any of the els which hav
+		 *  associated labels, remove/add the disabled class as appropriate.
 		 */
-		var inputEls = els.filter('input[id]');
 		if (isEnabled) {
 			els.removeAttr('disabled').removeClass('disabled');
-			inputEls.each(function(){
+			els.each(function(){
 				$('label[for="' + $(this).attr('id') + '"]').removeClass('disabled');
 			});
 		}
 		else {
 			els.attr('disabled', 'disabled').addClass('disabled');
-			inputEls.each(function(){
+			els.each(function(){
 				$('label[for="' + $(this).attr('id') + '"]').addClass('disabled');
 			});
 		}
