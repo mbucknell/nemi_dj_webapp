fdd1e4f4058966e6ffe0c732379c88ac46dc7831
Mary Bucknell <mbucknell@usgs.gov>
2013-04-24 16:14:15 +0000

    Moved the static directory to the nemi_project directory. Set the STATIC_ROOT with a trailing slash.

--- nemi/nemi_project/settings.py
+++ nemi/nemi_project/settings.py
@@ -70,14 +70,14 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_HOME, 'static')
+STATIC_ROOT = os.path.join(SITE_HOME, 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
+STATICFILES_DIRS = ((os.path.join(PROJECT_HOME, 'static'),)
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -223,3 +223,4 @@ if os.getenv('JENKINS_URL', False):
             HOST=os.getenv('DBA_SQL_HOST'),
             PORT=os.getenv('DBA_SQL_PORT')
             ))
+    
