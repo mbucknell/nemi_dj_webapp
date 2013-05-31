8d0a32406d18e9ebc713c09351bece3cefaad9c7
Mary Bucknell <mbucknell@usgs.gov>
2013-05-31 19:54:51 +0000

    Added a robots.txt url and created a robots.txt template file. Added Disallow directives for all search results pages, the sams site, the apex site, the admin site, and the accounts pages.

--- nemi/nemi_project/urls.py
+++ nemi/nemi_project/urls.py
@@ -25,6 +25,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+    url(r'^robots\.txt$', 
+        TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     
     url(r'^tinymce/', include('tinymce.urls')),
     
