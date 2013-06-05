bfb89dfd273b28f8fed94de14b779a045c2ff0fc
Mary Bucknell <mbucknell@usgs.gov>
2013-06-05 13:16:39 +0000

    Added task for running jenkins test and isolated running django commands so that the environment can be set up when on jenkins.

--- nemi/fabfile.py
+++ nemi/fabfile.py
@@ -5,6 +5,23 @@ from fabric.contrib.console import confirm
 
 import os
 
+
+def execute_django_command(command, for_deployment=False):
+    '''
+    When building for deployment there will not be a local_settings.py. The
+    django management commands require SECRET_KEY to be defined so create a
+    temporary local_settings, perform the command and then remove the local_settings.
+    The command parameter should be a string.
+    '''
+    
+    if for_deployment:
+        local('echo "SECRET_KEY = \'temporary key\'" > project_name/local_settings.py')
+
+    local('env/bin/python manage.py ' + command)
+    
+    if for_deployment:
+        local('rm nemi_project/local_settings.*');
+        
 @task
 def build_virtualenv(for_deployment=False):
     '''Assumes code has been retrieved from SVN'
@@ -32,21 +49,33 @@ def build_virtualenv(for_deployment=False):
         local('env/bin/pip --timeout=120 install -r requirements.txt')
 
         
-
 @task
 def build_project_env(for_deployment=False):
     '''Assumes code has already been retrieved from SVN and requirements installed in the virtualenv in env.
     '''
     # Collect static files
-    if for_deployment:
-        local('env/bin/python manage.py collectstatic --settings=nemi_project.settings');
+    execute_django_command('collectstatic --settings=nemi_project.settings', for_deployment)
     
     # Install compass and compile sass files
+    # Note that nemidjdev will always contain a copy of the latest css files in
+    # webapps/nemi/nemi_project/static/styles if you don't have Ruby installed.
     with lcd('compass'):
         local('./install.sh')
         local('./compass.sh compile')
-   
+        
+@task
+def run_jenkins_tests(for_deployment=False):
+    if not for_deployment:
+        abort('Can\'t run jenkins test on local development server')
+                    
+    with shell_env(DBA_SQL_DJANGO_ENGINE='django.db.backends.sqlite3'):
+        execute_django_command('jenkins', for_deployment)
+           
 @task
 def build(for_deployment=False):
     build_virtualenv(for_deployment)
+    
+    # Create dummy local_settings.py if for_deployment  
     build_project_env(for_deployment) 
+    
+    
