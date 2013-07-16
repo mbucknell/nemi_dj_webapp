25709c995d94682fa08225331d9a219531a1d676
Mary Bucknell <mbucknell@usgs.gov>
2013-07-16 20:36:47 +0000

    Added IE8 landing page and disable the Home link if IE8 or lower.

--- nemi/nemi_project/urls.py
+++ nemi/nemi_project/urls.py
@@ -28,6 +28,9 @@ urlpatterns = patterns('',
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
     url(r'^robots\.txt$', 
         TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^ie3_error/$', 
+        TemplateView.as_view(template_name='ie8_error_page.html'),
+        name='ie8_error_page'),
     
     url(r'^tinymce/', include('tinymce.urls')),
     
