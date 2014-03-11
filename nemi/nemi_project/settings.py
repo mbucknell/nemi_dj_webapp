034e36fd6ec830826114fadf794f6b108beb7e7f
Mary Bucknell <mbucknell@usgs.gov>
2014-03-11 15:39:38 +0000

    Django-jenkins app now uses standard Django test discovery so don't need to specify this task.

--- nemi/nemi_project/settings.py
+++ nemi/nemi_project/settings.py
@@ -214,7 +214,6 @@ TINYMCE_DEFAULT_CONFIG = {
 
 if os.getenv('JENKINS_URL', False):
     JENKINS_TASKS = (
-                     'django_jenkins.tasks.django_tests',
                      'django_jenkins.tasks.run_pylint',
                      'django_jenkins.tasks.with_coverage'
                      )
