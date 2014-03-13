4706070d80ab34eb41efc0ca629b7f052e0ff025
Mary Bucknell <mbucknell@usgs.gov>
2014-03-13 15:02:53 +0000

    Some changes to try to get coverage to work.

--- nemi/nemi_project/settings.py
+++ nemi/nemi_project/settings.py
@@ -214,10 +214,11 @@ TINYMCE_DEFAULT_CONFIG = {
 
 if os.getenv('JENKINS_URL', False):
     JENKINS_TASKS = (
+                    'django_jenkins.tasks.with_coverage'
                      'django_jenkins.tasks.run_pylint',
-                     'django_jenkins.tasks.with_coverage'
                      )
     JENKINS_TEST_RUNNER = 'nemi_project.test_jenkins_runner.ManagedModelTestRunner'
+    COVERAGE_EXCLUDES_FOLDERS = ('*/tests',)
     INSTALLED_APPS += ('django_jenkins',)
     PROJECT_APPS = ('common', 'methods', 'sams', 'newsfeed', 'domhelp')
     DATABASES['default'].update(dict(
