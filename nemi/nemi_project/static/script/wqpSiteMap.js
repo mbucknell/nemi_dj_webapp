21dadca7e9d9c0e6dc84aa9fb30d0e899fa34251
mbucknel <mbucknell@usgs.gov>
2014-04-14 15:41:16 -0500

    Refactored the wqpSiteMap javascript file so that the functions extend Leaflet classes for GeoJSON and map. Adjusted the tests. This decreased the amount of code and also decreased the number of tests required.

--- nemi/nemi_project/static/script/wqpSiteMap.js
+++ nemi/nemi_project/static/script/wqpSiteMap.js
@@ -2,69 +2,62 @@
 
 var WQP_MAP = WQP_MAP || {};
 
-WQP_MAP.siteLayer = function(spec) {
-	// spec (Object -optional) with the following properties:
-	//     serviceURL : (optional)The url to call to get the geoJSON file. Defaults to www.waterqualitydata.us.
-	//	   data : (optional)Object or String. Used to retrieve the geoJSON site data from WQP. If a string it is a query string.	
-	//     layerOptions : (optional) see Leaflet geojson docs. Default options will be used if not defined.
-	//	   errorHandler : (optional) function(jgXHR, textStatus, errorThrown)
-	
-	var idHtml = function (props) {
-		var result = '<table>' +
+/* New options defined are:
+ * 	   serviceURL : String containing the url to be used to fetch the site geoJSON data
+ * 	   data : String or Object containg the query parameter to be used in the ajax call
+ *     popupHtmlFromProperty : function(props(Object)) returns html string to be used for the identify popup
+ * Other options that are defaulted   
+ *     onEachFeature : function(feature, layer). By default this binds a popup to each feature. Can be overriden.
+ */
+WQP_MAP.WQPSitesLayer = L.GeoJSON.extend({
+	options : {
+		serviceURL : 'http://www.waterqualitydata.us/SimpleStation/search',
+		data  : {},
+		errorHandler : function() { return; },
+		popupHtmlFromProperty : function(props) {
+			var result = '<table>' +
 			'<tr><th>Site:</th><td>' + props.MonitoringLocationIdentifier + '</td></tr>' +
 			'<tr><th>Site type:</th><td>' + props.ResolvedMonitoringLocationTypeName + '</td></tr>/' +
 			'<tr><th>Data source:</th><td>' + props.ProviderName + '</td></tr></table>';
-		return result;
-	};
-	
-	DEFAULT_OPTIONS = {
-			onEachFeature : function(feature, layer){
-				layer.bindPopup(idHtml(feature.properties));
-			}
-	};
-	
-	spec = spec || {};
-	var that = L.geoJson([], $.extend({}, DEFAULT_OPTIONS, spec.layerOptions || {}));
-	
-	$.ajax({
-		url : spec.serviceURL || 'http://www.waterqualitydata.us/SimpleStation/search',
-		type : 'GET',
-		data : spec.data || {},
-		success : function(data, textStatus, jqXHR) {
-			that.addData(data.features);
+			return result;
 		},
-		error : spec.errorHandler || function() {return;}
-	});
-	
-	return that;
+	},
+	initialize : function(options) {
+		options = options || {};
+		if (!(options.onEachFeature)) {
+			options.onEachFeature = function(feature, layer) {
+				layer.bindPopup(options.popupHtmlFromProperty(feature.properties))
+			}
+		}
+		L.GeoJSON.prototype.initialize.call(this, [], options);
+				
+		$.ajax({
+			url : this.options.serviceURL,
+			type : 'GET',
+			data : this.options.data,
+			context : this,
+			success : function(data, textStatus, jqXHR) {
+				this.addData(data.features);
+			},
+			error : this.options.errorHandler
+		});
+	}
+});
+
+WQP_MAP.wqpSitesLayer = function(options) {
+	return new WQP_MAP.WQPSitesLayer(options);
 };
 
-WQP_MAP.siteMap = function(spec) {
-	// spec object can should have the following properties
-	//      mapDivId : The id of the div which will contain the site map
-	//      serviceURL : (optional)The url to call to get the geoJSON file. Defaults to www.waterqualitydata.us.
-	//		data : (optional)Object or String. Used to retrieve the geoJSON site data from WQP. If a string it is a query string. 
-	//		mapOptions : (optional) Object containing Leaflet map options. These will override any defaults.
-	//      layerOptions : (optional) see Leaflet geojson docs. Default options will be used if not defined.
-	// Returns an object containing the map created and the geojson layer. The layer has already been added to the map.
-	
-	var that = {};
-	spec.mapOptions = spec.mapOptions || {};
-	
-	var DEFAULT_MAP_OPTIONS = {
-			center : [49.2, -90.5],
-			zoom : 3,
-			attributionControl : false,
-			layers : [L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png')]
-	};
-	
-	that.map = L.map(spec.mapDivId, $.extend({}, DEFAULT_MAP_OPTIONS, spec.mapOptions));
-	that.siteLayer = WQP_MAP.siteLayer({
-		serviceURL : spec.serviceURL || null,
-		data : spec.data || null,
-		layerOptions : spec.layerOptions || null
-	});
-	that.map.addLayer(that.siteLayer);
-		
-	return that;	
+// Creates the default map to be used for mapping the sites.
+WQP_MAP.SiteMap = L.Map.extend({
+	options : {
+		center : [49.2, -90.5],
+		zoom : 3,
+		attributionControl : false,
+		layers : [L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png')]
+	}
+});
+
+WQP_MAP.siteMap = function(mapDivId, options) {
+	return new WQP_MAP.SiteMap(mapDivId, options)
 }
