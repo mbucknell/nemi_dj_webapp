cd10416fa23f61acaacaf56ff3247e097502735a
mbucknel <mbucknell@usgs.gov>
2014-05-08 08:28:19 -0500

    Added the browse protocols and protocol summary pages to the sitemap so that they are more discoverable by search engines.

--- nemi/nemi_project/sitemaps.py
+++ nemi/nemi_project/sitemaps.py
@@ -1,40 +1,53 @@
 
 from django.contrib.sitemaps import Sitemap;
-from django.core.urlresolvers import reverse
 
-from common.models import Method
+from common.models import Method, SourceCitationRef
 
 class MethodSitemap(Sitemap):
-    
+
     changefreq = 'monthly'
-    
+
     def items(self):
         return Method.objects.all().exclude(method_subcategory_id__in=[16, 17])
-    
+
     def location(self, obj):
-        return '/methods/method_summary/' + str(obj.method_id) +'/'
-    
+        return '/methods/method_summary/' + str(obj.method_id) + '/'
+
     def lastmod(self, obj):
         return obj.last_update_date
-    
-class StatisticalMethodSitemap(Sitemap):
-    
+
+
+class ProtocolSitemap(Sitemap):
     changefreq = 'monthly'
-    
+
     def items(self):
-        return Method.objects.all().filter(method_subcategory_id__in=[16,17])
-    
+        return SourceCitationRef.protocol_objects.all()
+
+    def location(self, obj):
+        return '/protocols/protocol_sumary/' + str(obj.source_citation_id) + '/'
+
+    def lastmod(self, obj):
+        return obj.update_date
+
+
+class StatisticalMethodSitemap(Sitemap):
+
+    changefreq = 'monthly'
+
+    def items(self):
+        return Method.objects.all().filter(method_subcategory_id__in=[16, 17])
+
     def location(self, obj):
         return '/methods/sams_method_summary/' + str(obj.method_id) + '/'
-    
+
     def lastmod(self, obj):
         return obj.last_update_date
 
 class StaticSitemap(Sitemap):
-    
+
     def items(self):
-        return ['/methods/browse_methods/', '/home/', '/glossary/']
-    
+        return ['/methods/browse_methods/', '/home/', '/glossary/', '/protocols/browse/']
+
     def location(self, item):
         return item
-    
+
