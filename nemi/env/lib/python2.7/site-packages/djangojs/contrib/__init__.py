898b1890f25dada14f27873ccb61f29968ead8ec
Tomcat Application Server <tomcat@cida-eros-nemidjdev.er.usgs.gov>
2015-04-09 12:09:32 -0500

    automatic exclusion of the env and static files

--- nemi/env/lib/python2.7/site-packages/djangojs/contrib/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-This package is dedicated to contributed compatibility modules.
-
-These modules provide mixins and already packed
-:class:`ContextSerializers <djangojs.context_serializer.ContextSerializer>`
-to use Django applications incompatible with the defaut Django.js behavior.
-'''
--- nemi/env/lib/python2.7/site-packages/djangojs/templates/djangojs/init.js
+++
@@ -1,4 +0,0 @@
-(function(){
-window.DJANGO_JS_URLS = {{ urls|safe }};
-window.DJANGO_JS_CONTEXT = {{ context|safe }};
-}());
