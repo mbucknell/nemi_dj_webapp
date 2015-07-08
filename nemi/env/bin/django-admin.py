58ad636bc2ea2930fa98bd76018a6c20f2681e04
Tomcat Application Server <tomcat@cida-eros-nemidjdev.er.usgs.gov>
2015-07-08 15:20:36 -0500

    automatic inclusion of the env and static files

---
+++ nemi/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/opt/tomcat/.jenkins/jobs/nemi_webapp_build/workspace/nemi/env/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
