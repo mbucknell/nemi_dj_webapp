b083ee2b90d55ea00f1c0fec0559b6ad99810a63
Daniel Naab <dnaab@usgs.gov>
2017-08-30 11:59:45 -0500

    Add admin config for method tables, and some model field meta. Clear some PEP8 warnings from pylint. Overly agressive whitespace changes from Sublime Text...

--- nemi/common/admin.py
+++ nemi/common/admin.py
@@ -1,25 +1,56 @@
-
 from django.contrib import admin
 from django.contrib.flatpages.models import FlatPage
 from django.forms import ModelForm
-
 from tinymce.widgets import TinyMCE
 
+from . import models
+
+
 class FlatPageForm(ModelForm):
-    
+
     class Meta:
         model = FlatPage
         fields = '__all__'
-        widgets = {'content' : TinyMCE(attrs={'cols' : 100, 
-                                              'rows': 50,
-                                              })
-                   }
-        
+        widgets = {'content': TinyMCE(attrs={'cols': 100, 'rows': 50})}
+
+
 class FlatPageAdmin(admin.ModelAdmin):
-    
+
     form = FlatPageForm
-    
-    
+
+
+class MethodAdmin(admin.ModelAdmin):
+    list_display = (
+        'method_official_name', 'insert_date', 'last_update_date',
+        'approved', 'approved_date'
+    )
+    class Meta:
+        model = models.Method
+        fields = '__all__'
+
+
+class MethodOnlineAdmin(admin.ModelAdmin):
+    list_display = (
+        'method_official_name', 'insert_date', 'last_update_date',
+        'approved', 'approved_date'
+    )
+    class Meta:
+        model = models.MethodOnline
+        fields = '__all__'
+
+
+class MethodStgAdmin(admin.ModelAdmin):
+    list_display = (
+        'method_official_name', 'insert_date', 'last_update_date',
+        'approved', 'approved_date'
+    )
+    class Meta:
+        model = models.MethodStg
+        fields = '__all__'
+
+
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
-
+admin.site.register(models.Method, MethodAdmin)
+admin.site.register(models.MethodOnline, MethodOnlineAdmin)
+admin.site.register(models.MethodStg, MethodStgAdmin)
--- nemi/common/context_processors.py
+++ nemi/common/context_processors.py
@@ -1,14 +1,14 @@
 from django.conf import settings
 
+
 def project_settings(request):
-    ''' 
+    '''
     Returns a dictionary with a settings key which contains the dictionary of
     settings that will be available in the context variable settings
     '''
-    
+
     # Specify which settings you want exposed.
     exposed_settings = ('GA_TRACKING_CODE',)
     settings_dict = dict((key, getattr(settings, key)) if hasattr(settings, key) else (key, '') for key in exposed_settings)
-    
+
     return {'settings': settings_dict}
-    
