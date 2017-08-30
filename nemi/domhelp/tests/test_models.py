539bae52f5b4e2b8d3ff5e2287fc1549f4019659
Daniel Naab <dnaab@usgs.gov>
2017-08-30 15:33:03 -0500

    Django 1.9: Use TestCase rather than SimpleTestCase due to database mutation.

--- nemi/domhelp/tests/test_models.py
+++ nemi/domhelp/tests/test_models.py
@@ -1,25 +1,26 @@
 from django.core.exceptions import ValidationError
-from django.test import SimpleTestCase
+from django.test import TestCase
 
 from ..models import HelpContent
 
-class FieldNameValidatorTestCase(SimpleTestCase):
-    
+
+class FieldNameValidatorTestCase(TestCase):
+
     def test_valid_field_name(self):
         c1 = HelpContent.objects.create(field_name='my1_name', label='My Name')
 
         self.assertIsNone(c1.full_clean())
-        
+
     def test_leading_underscore_field_name(self):
         c1 = HelpContent.objects.create(field_name='_my1_name', label='My Name')
         with self.assertRaises(ValidationError):
             c1.full_clean()
-        
+
     def test_trailing_underscore_field_name(self):
         c1 = HelpContent.objects.create(field_name='my1_name_', label='My Name')
         with self.assertRaises(ValidationError):
             c1.full_clean()
-        
+
     def test_non_alphanumeric_characters_field_name(self):
         c1 = HelpContent.objects.create(field_name='my%_name', label='My Name')
         with self.assertRaises(ValidationError):
