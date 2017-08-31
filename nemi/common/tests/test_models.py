a2cd467013fab57e70b501b03e86cb12b7616822
Daniel Naab <dnaab@usgs.gov>
2017-08-31 08:38:54 -0500

    Fix failing test case. (test_get_insert_user was previously indented an extra level deep, and never executed)

--- nemi/common/tests/test_models.py
+++ nemi/common/tests/test_models.py
@@ -10,31 +10,39 @@ from django.test import TestCase
 
 from factory.django import DjangoModelFactory
 
-from common.models import MethodAbstract, StatisticalItemType, MethodTypeRef, SourceCitationRef
+from common.models import MethodAbstract, StatisticalItemType, MethodTypeRef, SourceCitationRef, InstrumentationRef
 
 
 class TestMethodGetInsertUser(TestCase):
 
     class DummyMethod(MethodAbstract):
-        dummyField = models.IntegerField()
+        dummy_field = models.IntegerField()
 
     def setUp(self):
-        self.u1 = User.objects.create('user1', password='password1')
-        self.u2 = User.objects.create('user2', password='password2')
+        self.u1 = User.objects.create_user('user1', 'user1@email', 'password1')
+        self.u2 = User.objects.create_user('user2', 'user2@email', 'password2')
 
+        self.instrumentation = InstrumentationRef.objects.create(
+            instrumentation_id=1,
+            instrumentation='instrumentation',
+            instrumentation_description='description')
         self.item_type = StatisticalItemType.objects.create(stat_item_index=1, item='Stat Item')
         self.method_type = MethodTypeRef.objects.create(method_type_id=1, method_type_desc='method_type_desc')
 
-        self.m1 = self.DummyMethod.objects.create(source_method_identifier='id1',
+        self.m1 = self.DummyMethod.objects.create(dummy_field=1,
+                                                  source_method_identifier='id1',
                                                   method_official_name='name1',
                                                   brief_method_summary='summary1',
                                                   insert_person_name='user1',
-                                                  method_type=self.method_type)
-        self.m2 = self.DummyMethod.objects.create(source_method_identifier='id2',
+                                                  method_type=self.method_type,
+                                                  instrumentation=self.instrumentation)
+        self.m2 = self.DummyMethod.objects.create(dummy_field=1,
+                                                  source_method_identifier='id2',
                                                   method_official_name='name2',
                                                   brief_method_summary='summary2',
                                                   insert_person_name='user3',
-                                                  method_type=self.method_type)
+                                                  method_type=self.method_type,
+                                                  instrumentation=self.instrumentation)
 
     def test_get_insert_user(self):
         self.assertEqual(self.m1.get_insert_user(), self.u1)
