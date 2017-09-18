a1e2777b1cf85330f6018d25ef143e150f632916
Daniel Naab <dnaab@usgs.gov>
2017-09-18 15:15:49 -0500

    NEMI-396 - Manage Analytes UI

--- nemi/reference/admin.py
+++ nemi/reference/admin.py
@@ -7,6 +7,19 @@ from nemi_project.admin import method_admin
 from . import models
 
 
+class UserTimestampMixin:
+    fields = (
+        'data_entry_name', 'data_entry_date', 'update_name', 'update_date'
+    )
+    readonly_fields = (
+        'data_entry_name', 'data_entry_date', 'update_name', 'update_date'
+    )
+
+    def save_model(self, request, obj, form, change):
+        self.audit_instance(request.user.username, obj)
+        return super(UserTimestampMixin, self).save_model(request, obj, form, change)
+
+
 class ReferenceTableAdmin(admin.ModelAdmin):
     class Meta:
         abstract = True
@@ -16,6 +29,13 @@ class ReferenceTableAdmin(admin.ModelAdmin):
             self._is_admin = request.user.groups.filter(
                 name='method_admin').exists()
         return self._is_admin
+
+    def audit_instance(self, username, obj):
+        obj.update_name = username
+        obj.update_date = datetime.now()
+        if not obj.pk:
+            obj.data_entry_name = username
+            obj.data_entry_date = obj.update_date
 
     has_module_permission = is_method_admin
     has_add_permission = is_method_admin
@@ -46,13 +66,46 @@ class AccuracyUnitsDomAdmin(ReferenceTableAdmin):
     )
 
     def save_model(self, request, obj, form, change):
-        if change:
-            obj.update_name = request.user.username
-            obj.update_date = datetime.now()
-        else:
-            obj.data_entry_name = request.user.username
-            obj.update_date = datetime.now()
+        self.audit_instance(request.user.username, obj)
         return super(AccuracyUnitsDomAdmin, self).save_model(request, obj, form, change)
 
 
+class AnalyteCodeRelAdmin(UserTimestampMixin, admin.TabularInline):
+    model = models.AnalyteCodeRel
+    extra = 0
+    fields = ('analyte_name', 'preferred') + UserTimestampMixin.fields
+
+    def save_model(self, request, obj, form, change):
+        self.audit_instance(request, obj, change)
+        return super(AnalyteCodeRelAdmin, self).save_model(request, obj, form, change)
+
+
+class AnalyteRefAdmin(UserTimestampMixin, ReferenceTableAdmin):
+    class Meta:
+        model = models.AnalyteRef
+
+    inlines = (AnalyteCodeRelAdmin,)
+    list_display = ('analyte_code', 'analyte_type', 'analyte_cbr')
+    fields = (
+        'analyte_code', 'analyte_type', 'analyte_cbr', 'usgs_pcode',
+        'analyte_code_cbr',
+        # These two are in the database but not in the APEX UI:
+        #'cbr_analyte_category', 'cbr_analyte_intro'
+    ) + UserTimestampMixin.fields
+
+    def save_model(self, request, obj, form, change):
+        self.audit_instance(request.user.username, obj)
+        return super(AnalyteRefAdmin, self).save_model(request, obj, form, change)
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for instance in formset.deleted_objects:
+            instance.delete()
+        for instance in instances:
+            self.audit_instance(request.user.username, instance)
+            instance.save()
+        formset.save_m2m()
+
+
 method_admin.register(models.AccuracyUnitsDom, AccuracyUnitsDomAdmin)
+method_admin.register(models.AnalyteRef, AnalyteRefAdmin)
--- nemi/reference/models.py
+++ nemi/reference/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+YES_NO_CHOICES = (
+    ('N', 'No'),
+    ('Y', 'Yes')
+)
+
+
 class AccuracyUnitsDom(models.Model):
     accuracy_units = models.CharField(primary_key=True, max_length=50)
     accuracy_units_description = models.CharField(max_length=50, blank=True, null=True)
@@ -16,3 +22,82 @@ class AccuracyUnitsDom(models.Model):
 
     def __str__(self):
         return self.accuracy_units
+
+
+class CbrAdvice(models.Model):
+    advice_id = models.IntegerField(primary_key=True)
+    analyte_category = models.CharField(max_length=1000)
+    analyte_category_intro = models.CharField(max_length=4000, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'cbr_advice'
+
+
+class AnalyteRef(models.Model):
+    analyte_id = models.IntegerField(primary_key=True)
+
+    analyte_code = models.CharField(unique=True, max_length=20)
+    analyte_type = models.CharField(max_length=100, blank=True, null=True)
+    analyte_cbr = models.CharField(
+        max_length=1,
+        blank=True,
+        null=True,
+        verbose_name='analyte CBR',
+        choices=YES_NO_CHOICES)
+    usgs_pcode = models.CharField(
+        max_length=6,
+        blank=True,
+        null=True,
+        verbose_name='USGS pcode')
+    analyte_code_cbr = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+        verbose_name='analyte code CBR',
+        choices=YES_NO_CHOICES)
+
+    cbr_analyte_category = models.ForeignKey(CbrAdvice, models.DO_NOTHING, db_column='cbr_analyte_category', blank=True, null=True)
+    cbr_analyte_intro = models.CharField(max_length=4000, blank=True, null=True)
+
+    data_entry_name = models.CharField(max_length=50, blank=True, null=True)
+    data_entry_date = models.DateField(blank=True, null=True)
+    update_name = models.CharField(max_length=50, blank=True, null=True)
+    update_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'analyte_ref'
+        verbose_name = 'analyte'
+
+    def __str__(self):
+        return self.analyte_code
+
+
+class AnalyteCodeRel(models.Model):
+    analyte_code_id = models.AutoField(primary_key=True)
+    analyte = models.ForeignKey(AnalyteRef, models.DO_NOTHING)
+
+    analyte_code = models.CharField(max_length=20, blank=True, null=True)
+    analyte_name = models.CharField(max_length=240)
+    preferred = models.IntegerField(
+        blank=True,
+        null=True,
+        choices=((None, 'No'), (-1, 'Yes'))
+    )
+    analyte_type = models.CharField(max_length=50, blank=True, null=True)
+
+    data_entry_name = models.CharField(max_length=20, blank=True, null=True)
+    data_entry_date = models.DateField(blank=True, null=True)
+    update_name = models.CharField(max_length=50, blank=True, null=True)
+    update_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'analyte_code_rel'
+        unique_together = (('analyte', 'analyte_name'),)
+        verbose_name = 'analyte and code management'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.analyte_name
