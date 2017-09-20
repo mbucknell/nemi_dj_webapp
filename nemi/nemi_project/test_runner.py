80144acc050da01e3435e0542e9bfa9928030667
Daniel Naab <dnaab@usgs.gov>
2017-09-20 13:59:44 -0500

    Update test runner to only treat one model per db table as the "managed" version. This fixes issues with duplicate models created for the admin interface - the alternative is to modify other old code that is dependent on models having only a subset of all columns on the table.

--- nemi/nemi_project/test_runner.py
+++ nemi/nemi_project/test_runner.py
@@ -3,7 +3,23 @@ Created on Mar 14, 2012
 
 @author: mbucknel
 '''
+from collections import defaultdict
+
+from django.apps import apps
 from django.test.runner import DiscoverRunner
+
+
+def field_count(model_class):
+    return len([
+        f
+        for f in model_class._meta.get_fields()
+        if f.concrete and (
+            not f.is_relation
+            or f.one_to_one
+            or (f.many_to_one and f.related_model)
+        )
+    ])
+
 
 # I got this snippet from http://www.caktusgroup.com/blog/2010/09/24/simplifying-the-testing-of-unmanaged-database-models-in-django/
 class ManagedModelTestRunner(DiscoverRunner):
@@ -13,12 +29,22 @@ class ManagedModelTestRunner(DiscoverRunner):
     to execute the SQL manually to create them.
     """
     def setup_test_environment(self, *args, **kwargs):
-        from django.apps import apps
-        get_models = apps.get_models
-        self.unmanaged_models = [m for m in get_models(include_auto_created=True)
-                                 if not m._meta.managed]
+        # To handle the case where we have more than one unmanaged model per
+        # table, treat the model with more fields as the "managed" version.
+
+        models_by_table = defaultdict(list)
+        for model in apps.get_models(include_auto_created=True):
+            lst = models_by_table.setdefault(model._meta.db_table, [])
+            lst.append(model)
+
+        self.unmanaged_models = [
+            max(models, key=field_count)
+            for _, models in models_by_table.items()
+        ]
+
         for m in self.unmanaged_models:
             m._meta.managed = True
+
         super(ManagedModelTestRunner, self).setup_test_environment(*args,
                                                                    **kwargs)
 
