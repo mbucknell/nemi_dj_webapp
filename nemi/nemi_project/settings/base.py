da9e60da5d798e1b242c40226607713a63b46b54
Daniel Naab <dnaab@usgs.gov>
2017-09-22 13:05:45 -0500

    Put static files in project root, to be consistent with ansible_webapp_flask_deploy.

--- nemi/nemi_project/settings/base.py
+++ nemi/nemi_project/settings/base.py
@@ -73,7 +73,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_HOME, 'static/')
+STATIC_ROOT = os.path.join(SITE_HOME, '../static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
